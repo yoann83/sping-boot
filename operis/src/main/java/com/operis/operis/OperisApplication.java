c3769f9555f23e1390f32e578fd6da230daa5f60
Yoann <martinezyoann83@gmail.com>
2026-01-07 11:30:46 +0100

    initialized operis project

---
+++ operis/src/main/java/com/operis/operis/OperisApplication.java
@@ -0,0 +1,13 @@
+package com.operis.operis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperisApplication.class, args);
+	}
+
+}
---
+++ operis/src/test/java/com/operis/operis/OperisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operis.operis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
